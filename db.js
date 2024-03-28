246d002525fc9c9e2e3358016cd98171ebf6caa7
roman thapa <thaparoman970@gmail.com>
2024-03-28 21:55:15 +0545

    update to mongodbatlas

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mongooes = require("mongoose");
-const mongourl = "mongodb://localhost:27017/?directConnection=true";
+const mongourl = "mongodb+srv://thaparoman970:o8uh3kRmWEl9HW16@cluster0.7a2motw.mongodb.net/";
 
 //this function helps to connect to the mangodb database through the url
 const connectToMongoo = async () => {
   try {
-    mongooes.connect(mongourl);
-    console.log("connected to monogooes succesfully");
+    // Connect to MongoDB Atlas
+    await mongooes.connect(mongourl);
+    console.log("connected to mongodb atlas succesfully");
   } catch (error) {
     console.log(error);
   }
